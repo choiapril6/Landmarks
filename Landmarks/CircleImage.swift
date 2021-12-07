e725d45410a112ae6b05e95519b04441d692be08
Jane Choi <jane.choi@navercorp.com>
2021-12-07 23:20:56 +0900

    01.section4

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Jane Choi on 2021/12/07.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        VStack {
+            Image("turtlerock", bundle: nil)
+                .clipShape(Circle())
+                .overlay {
+                    Circle().stroke(.white, lineWidth: 4)
+                }
+                .shadow(radius: 7)
+        }
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
